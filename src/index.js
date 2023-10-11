94abdf0e669ff54f98558de06e8d5f9162b0616d
Konst_V <varjutin@gmail.com>
2023-10-12 01:31:13 +0300

    delete code from index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,1 @@
 import "./styles/index.css";
-
-const userStack = {
-  language: "Javascript",
-  framework: "React",
-};
-
-const user = {
-  name: "Konst",
-  age: 38,
-  ...userStack,
-};
-
-const user2 = {
-  work: "Frontend development",
-  ...user,
-};
-
-console.log(user2);
